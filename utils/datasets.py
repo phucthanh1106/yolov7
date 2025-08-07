ab4a235404eb58b7ce41880ebabaa29c9154a3c5
phucthanh1106 <115474769+phucthanh1106@users.noreply.github.com>
2025-08-07 13:57:14 +0700

    Update copy_paste to only work with bike objects

--- utils/datasets.py
+++ utils/datasets.py
@@ -897,21 +897,28 @@ def copy_paste(img, labels, segments, probability=0.5):
     if probability and n:
         h, w, c = img.shape  # height, width, channels
         im_new = np.zeros(img.shape, np.uint8)
-        for j in random.sample(range(n), k=round(probability * n)):
-            l, s = labels[j], segments[j]
-            box = w - l[3], l[2], w - l[1], l[4]
-            ioa = bbox_ioa(box, labels[:, 1:5])  # intersection over area
-            if (ioa < 0.30).all():  # allow 30% obscuration of existing labels
-                labels = np.concatenate((labels, [[l[0], *box]]), 0)
-                segments.append(np.concatenate((w - s[:, 0:1], s[:, 1:2]), 1))
-                cv2.drawContours(im_new, [segments[j].astype(np.int32)], -1, (255, 255, 255), cv2.FILLED)
-
-        result = cv2.bitwise_and(src1=img, src2=im_new)
-        result = cv2.flip(result, 1)  # augment segments (flip left-right)
-        i = result > 0  # pixels to replace
-        # i[:, :] = result.max(2).reshape(h, w, 1)  # act over ch
-        img[i] = result[i]  # cv2.imwrite('debug.jpg', img)  # debug
-
+        
+        bike_indices = [] # Getting all bike objects in this image
+        for j, label in enumerate(labels):
+            if int(label[0]) == 0:
+                bike_indices.append(j)
+        
+        if len(bike_indices) != 0:
+            for j in bike_indices:
+                l, s = labels[j], segments[j]
+                box = w - l[3], l[2], w - l[1], l[4]
+                ioa = bbox_ioa(box, labels[:, 1:5])  # intersection over area
+                if (ioa < 0.30).all():  # allow 30% obscuration of existing labels
+                    labels = np.concatenate((labels, [[l[0], *box]]), 0)
+                    segments.append(np.concatenate((w - s[:, 0:1], s[:, 1:2]), 1))
+                    cv2.drawContours(im_new, [segments[j].astype(np.int32)], -1, (255, 255, 255), cv2.FILLED)
+    
+            result = cv2.bitwise_and(src1=img, src2=im_new)
+            result = cv2.flip(result, 1)  # augment segments (flip left-right)
+            i = result > 0  # pixels to replace
+            # i[:, :] = result.max(2).reshape(h, w, 1)  # act over ch
+            img[i] = result[i]  # cv2.imwrite('debug.jpg', img)  # debug
+    
     return img, labels, segments
 
 
